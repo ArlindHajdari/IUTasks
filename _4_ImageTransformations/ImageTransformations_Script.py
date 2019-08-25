99f54b2d3d852f4165054c6bd012ad30c70b9f8c
Arlind Hajdari <arlind.hajdari@hotmail.com>
2019-08-25 21:20:43 +0200

    Final changes to Image Transformation

--- _4_ImageTransformations/ImageTransformations_Script.py
+++ _4_ImageTransformations/ImageTransformations_Script.py
@@ -52,10 +52,20 @@ def breakImage(image, blocks = 16):
 
     return blocked_image
 
+def lowResolution(image):
+    x, y = image.shape
+    low_resolution_image = np.zeros((x,y))
+
+    for i in range(0, x):
+        for j in range(0, y):
+            low_resolution_image[i,j] = image[i,j].mean()
+
+    return low_resolution_image
+
 def main():
     # ============= Negative image ================
-    # img = mpimg.imread("../test_images/lena.png")
-    # img_negative = get_negative_img(img)
+    img = mpimg.imread("../test_images/lena.png")
+    img_negative = get_negative_img(img)
 
     # fig, (axis1, axis2) = plt.subplots(1, 2)
     # fig.suptitle("Negativity")
@@ -66,7 +76,7 @@ def main():
 
     # ============= Mirroring image ===============
     # img = mpimg.imread("../test_images/lena.png")
-    # img_mirror = np.fliplr(img)
+    img_mirror = np.fliplr(img_negative)
 
     # fig, (axis1, axis2) = plt.subplots(1, 2)
     # fig.suptitle("Mirroring")
@@ -77,9 +87,9 @@ def main():
 
     # ============= Add/Substraction of image =====
     # img = mpimg.imread("../test_images/lena.png")
-    # random_value = randint(0,256)
-    # img_transformed_pixels = transorm_pixels(img, random_value)
-    # clipped_image = clip_img(img_transformed_pixels, 0, 255)
+    random_value = randint(0,256)
+    img_transformed_pixels = transorm_pixels(img_mirror, random_value)
+    clipped_image = clip_img(img_transformed_pixels, 0, 255)
 
     # fig, (axis1, axis2) = plt.subplots(1, 2)
     # fig.suptitle("Transforming Add/Substraction with value: "+ str(random_value))
@@ -90,48 +100,36 @@ def main():
 
     # ============= Tear image into 16x16 blocks and compute histogram =====
     # img = mpimg.imread("../test_images/lena.png")
-    # block_size = 16
-    # teared_up_image = breakImage(img, block_size)
-    # teared_up_image_with_histograms = np.zeros((block_size, block_size))
+    block_size = 16
+    teared_up_image = breakImage(img, block_size)
+    teared_up_image_with_histograms = np.zeros((block_size, block_size), dtype=object)
 
-    # for i in range(0, block_size):
-    #     for j in range(0, block_size):
-    #         teared_up_image_with_histograms[i,j] = imhist(teared_up_image[i,j].flatten(), 256)
-    
-    # print(teared_up_image_with_histograms)
+    for i in range(0, block_size):
+        for j in range(0, block_size):
+            teared_up_image_with_histograms[i,j] = imhist(teared_up_image[i,j].flatten(), 256)
 
-    # fig, ax = plt.subplots(nrows=16, ncols=16)
-    # fig.suptitle("Tear image into 16x16 blocks and compute histogram")
-    # plt.ion()
-    # i = 0
-    # for row in ax:
-    #     j = 0
-    #     for col in row:
-    #         col.plot(teared_up_image_with_histograms[i][j])
-    #         print(teared_up_image_with_histograms[i][j])
-    #         j = j + 1
-    #     i = i + 1
+    fig, ax = plt.subplots(nrows=block_size, ncols=block_size)
+    fig.suptitle(f"Tear image into {block_size}x{block_size} blocks and compute histogram")
+    plt.ion()
+    i = 0
+    for row in ax:
+        j = 0
+        for col in row:
+            col.plot(teared_up_image_with_histograms[i][j])
+            j = j + 1
+        i = i + 1
     
-    # plt.show(block=True)
+    plt.show(block=True)
 
     # ================= Low Resolution image =======================
-    img = mpimg.imread("../test_images/lena.png")
-    teared_up_image = breakImage(img, blocks = 16)
+    # img = mpimg.imread("../test_images/lena.png")
+    # teared_up_image = breakImage(img, blocks = 16)
     low_resolution_image = lowResolution(teared_up_image)
     
     plt.ion()
     plt.imshow(low_resolution_image, cmap='gray')
     plt.show(block=True)
 
-def lowResolution(image):
-    x, y = image.shape
-    low_resolution_image = np.zeros((x,y))
-
-    for i in range(0, x):
-        for j in range(0, y):
-            low_resolution_image[i,j] = image[i,j].mean()
-
-    return low_resolution_image
 
 if __name__ == "__main__":
     main()
