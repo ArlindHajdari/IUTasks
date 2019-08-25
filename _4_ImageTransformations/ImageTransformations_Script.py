f1716156730787409469c3f83dde230638f8d947
Arlind Hajdari <arlind.hajdari@hotmail.com>
2019-08-25 15:21:31 +0200

    Image Transformation almost completed

--- _4_ImageTransformations/ImageTransformations_Script.py
+++ _4_ImageTransformations/ImageTransformations_Script.py
@@ -41,17 +41,28 @@ def clip_img(image, minVal, maxVal):
 
     return image
 
+def breakImage(image, blocks = 16):
+    blocked_image = np.zeros((blocks, blocks), dtype=object)
+    x_split_by_percentage = image.shape[0]//blocks
+    y_split_by_percentage = image.shape[1]//blocks
+    
+    for i in range(0, image.shape[0] - x_split_by_percentage + 1, x_split_by_percentage):
+        for j in range(0, image.shape[1] - y_split_by_percentage + 1, y_split_by_percentage):
+            blocked_image[i//x_split_by_percentage,j//y_split_by_percentage] = image[i: i + blocks, j: j + blocks]
+
+    return blocked_image
+
 def main():
     # ============= Negative image ================
-    img = mpimg.imread("../test_images/lena.png")
-    img_negative = get_negative_img(img)
+    # img = mpimg.imread("../test_images/lena.png")
+    # img_negative = get_negative_img(img)
 
-    fig, (axis1, axis2) = plt.subplots(1, 2)
-    fig.suptitle("Negativity")
-    plt.ion()
-    axis1.imshow(img, cmap = "gray")
-    axis2.imshow(img_negative, cmap = "gray")
-    plt.show(block=True)
+    # fig, (axis1, axis2) = plt.subplots(1, 2)
+    # fig.suptitle("Negativity")
+    # plt.ion()
+    # axis1.imshow(img, cmap = "gray")
+    # axis2.imshow(img_negative, cmap = "gray")
+    # plt.show(block=True)
 
     # ============= Mirroring image ===============
     # img = mpimg.imread("../test_images/lena.png")
@@ -77,6 +88,50 @@ def main():
     # axis2.imshow(clipped_image, cmap = "gray")
     # plt.show(block=True)
 
+    # ============= Tear image into 16x16 blocks and compute histogram =====
+    # img = mpimg.imread("../test_images/lena.png")
+    # block_size = 16
+    # teared_up_image = breakImage(img, block_size)
+    # teared_up_image_with_histograms = np.zeros((block_size, block_size))
+
+    # for i in range(0, block_size):
+    #     for j in range(0, block_size):
+    #         teared_up_image_with_histograms[i,j] = imhist(teared_up_image[i,j].flatten(), 256)
+    
+    # print(teared_up_image_with_histograms)
+
+    # fig, ax = plt.subplots(nrows=16, ncols=16)
+    # fig.suptitle("Tear image into 16x16 blocks and compute histogram")
+    # plt.ion()
+    # i = 0
+    # for row in ax:
+    #     j = 0
+    #     for col in row:
+    #         col.plot(teared_up_image_with_histograms[i][j])
+    #         print(teared_up_image_with_histograms[i][j])
+    #         j = j + 1
+    #     i = i + 1
+    
+    # plt.show(block=True)
+
+    # ================= Low Resolution image =======================
+    img = mpimg.imread("../test_images/lena.png")
+    teared_up_image = breakImage(img, blocks = 16)
+    low_resolution_image = lowResolution(teared_up_image)
+    
+    plt.ion()
+    plt.imshow(low_resolution_image, cmap='gray')
+    plt.show(block=True)
+
+def lowResolution(image):
+    x, y = image.shape
+    low_resolution_image = np.zeros((x,y))
+
+    for i in range(0, x):
+        for j in range(0, y):
+            low_resolution_image[i,j] = image[i,j].mean()
+
+    return low_resolution_image
 
 if __name__ == "__main__":
     main()
