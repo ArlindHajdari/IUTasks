e15fb85f3d990dee6626c9a8a98e4a45d6f11866
Arlind Hajdari <arlind.hajdari@hotmail.com>
2019-08-27 20:08:50 +0200

    Image Transformations finished

--- _4_ImageTransformations/ImageTransformations_Script.py
+++ _4_ImageTransformations/ImageTransformations_Script.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from random import randint
 
+
 def get_negative_img(image):
     x, y = image.shape
     negative = np.zeros((x, y))
@@ -41,6 +42,7 @@ def clip_img(image, minVal, maxVal):
 
     return image
 
+
 def breakImage(image, blocks = 16):
     blocked_image = np.zeros((blocks, blocks), dtype=object)
     x_split_by_percentage = image.shape[0]//blocks
@@ -52,54 +54,47 @@ def breakImage(image, blocks = 16):
 
     return blocked_image
 
+
 def lowResolution(image):
     x, y = image.shape
     low_resolution_image = np.zeros((x,y))
 
     for i in range(0, x):
         for j in range(0, y):
-            low_resolution_image[i,j] = image[i,j].mean()
+            low_resolution_image[i,j] = int(255 * image[i,j].mean())
 
     return low_resolution_image
 
+
+def applyThreshold(image, threshold):
+    x, y = image.shape
+    thresholdedImage = np.zeros((x, y))
+
+    for i in range(0, x):
+        for j in range(0, y):
+            if image[i,j] > threshold:
+                thresholdedImage[i, j] = 0 
+            else:
+                thresholdedImage[i, j] = image[i,j]
+
+    return thresholdedImage
+
+
 def main():
-    # ============= Negative image ================
     img = mpimg.imread("../test_images/lena.png")
+    
+    # ============= Negative image ================
     img_negative = get_negative_img(img)
 
-    # fig, (axis1, axis2) = plt.subplots(1, 2)
-    # fig.suptitle("Negativity")
-    # plt.ion()
-    # axis1.imshow(img, cmap = "gray")
-    # axis2.imshow(img_negative, cmap = "gray")
-    # plt.show(block=True)
-
     # ============= Mirroring image ===============
-    # img = mpimg.imread("../test_images/lena.png")
     img_mirror = np.fliplr(img_negative)
 
-    # fig, (axis1, axis2) = plt.subplots(1, 2)
-    # fig.suptitle("Mirroring")
-    # plt.ion()
-    # axis1.imshow(img, cmap = "gray")
-    # axis2.imshow(img_mirror, cmap = "gray")
-    # plt.show(block=True)
-
     # ============= Add/Substraction of image =====
-    # img = mpimg.imread("../test_images/lena.png")
     random_value = randint(0,256)
     img_transformed_pixels = transorm_pixels(img_mirror, random_value)
     clipped_image = clip_img(img_transformed_pixels, 0, 255)
 
-    # fig, (axis1, axis2) = plt.subplots(1, 2)
-    # fig.suptitle("Transforming Add/Substraction with value: "+ str(random_value))
-    # plt.ion()
-    # axis1.imshow(img, cmap = "gray")
-    # axis2.imshow(clipped_image, cmap = "gray")
-    # plt.show(block=True)
-
     # ============= Tear image into 16x16 blocks and compute histogram =====
-    # img = mpimg.imread("../test_images/lena.png")
     block_size = 16
     teared_up_image = breakImage(img, block_size)
     teared_up_image_with_histograms = np.zeros((block_size, block_size), dtype=object)
@@ -122,13 +117,23 @@ def main():
     plt.show(block=True)
 
     # ================= Low Resolution image =======================
-    # img = mpimg.imread("../test_images/lena.png")
-    # teared_up_image = breakImage(img, blocks = 16)
     low_resolution_image = lowResolution(teared_up_image)
     
     plt.ion()
-    plt.imshow(low_resolution_image, cmap='gray')
+    plt.imshow(low_resolution_image, cmap = 'gray')
     plt.show(block=True)
+
+    # ================= Segment the face of Lena ===================
+    while True:
+        threshold = input("Type the threshold(0 to 255)<type exit to break>: ")
+
+        if "exit" not in threshold:
+            thresholded_image = applyThreshold(low_resolution_image, int(threshold))
+            plt.ion()
+            plt.imshow(thresholded_image, cmap = 'gray')
+            plt.show(block=True)
+        else:
+            break
 
 
 if __name__ == "__main__":
